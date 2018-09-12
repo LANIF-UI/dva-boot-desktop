bbbfb71e3ed5b1e352176fdae230eeac34187fd8
weiq <6682220aabc@163.com>
2018-09-12 17:12:40 +0800

    init

---
+++ config-overrides.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less-modules');
+
+module.exports = function override(config, env) {
+  config.resolve = {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      components: path.resolve(__dirname, 'src/components'),
+      assets: path.resolve(__dirname, 'src/assets')
+    }
+  };
+
+  if (env === 'development') {
+    config = injectBabelPlugin(['dva-hmr'], config);
+  } else {
+    config.output.publicPath = '/';
+  }
+
+  config = injectBabelPlugin('transform-decorators-legacy', config);
+  config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', style: true }],
+    config
+  );
+
+  config.externals = {};
+
+  return rewireLess.withLoaderOptions(
+    `${env === 'production' ? 'app' : '[local]'}-[hash:base64:8]`,
+    {
+      modifyVars: {}
+    }
+  )(config, env);
+};
