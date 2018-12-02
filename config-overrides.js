8002d7cbde2b1327dcedd58042b1f9ef270e3cf2
魏小雨 <6682220aabc@163.com>
2018-12-02 22:03:41 +0800

    update

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,33 @@
 const path = require('path');
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less-modules');
+const removeWebpackPlugins = require('react-app-rewire-unplug');
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 module.exports = function override(config, env) {
   config.target = 'electron-renderer';
-  
+
   config.resolve = {
     alias: {
       '@': path.resolve(__dirname, 'src'),
       components: path.resolve(__dirname, 'src/components'),
       assets: path.resolve(__dirname, 'src/assets'),
       package: path.resolve(__dirname, 'package.json'),
-      utils: path.resolve(__dirname, 'src/utils'),
+      utils: path.resolve(__dirname, 'src/utils')
     }
   };
 
   if (env === 'development') {
     config = injectBabelPlugin(['dva-hmr'], config);
+  } else {
+    config = removeWebpackPlugins(config, env, {
+      pluginNames: ['UglifyJsPlugin']
+    });
+    config.plugins = (config.plugins || []).concat([
+      new ParallelUglifyPlugin({
+        uglifyES: {}
+      })
+    ]);
   }
 
   config = injectBabelPlugin('transform-decorators-legacy', config);
